f16cd47d7654c3a3af7a2160cf5926986b8ecc32
rotaryivy <rotaryivy@163.com>
2018-07-02 21:53:19 +0800

    MF: Add killing process function

--- start_server.sh
+++ start_server.sh
@@ -13,14 +13,26 @@ createTopics(){
 }
 
 #判断是否有进程启动，如果有则kill
+killProc(){
+	pid=`jps -lm | grep org.apache.zookeeper.server.quorum.QuorumPeerMain | awk '{print $1}'`
+	kill -9 $pid
+
+	pid=`jps -lm | grep kafka.Kafka | awk '{print $1}'`
+	kill -9 $pid
+}
+
+#判断zookeeper是否启动成功
+checkZookeeper(){
+	pid=`jps -lm | grep org.apache.zookeeper.server.quorum.QuorumPeerMain | awk '{print $1}'`
+}
 
 
 #启动
 nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties>zookeeper-server-start.log 2>&1&
-echo "******************log folder: "$kafka.logs.dir
+echo "******************log folder: "${kafka.logs.dir}
 if [ $? -eq 0 ]; then
 	sleep 20s
-    nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties>kafka-server-start.log 2>&1&
+	nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties>kafka-server-start.log 2>&1&
     if [ $? -eq 0 ]; then
 		sleep 20s
     	createTopics
